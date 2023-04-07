fb2f3db4391c17b934529e9b7156bd688b2383e1
anjo233 <anjo233@outlook.com>
2023-04-07 12:25:40 +0800

    demo1

---
+++ code/dome.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import  time 
+
+st.write("计算进度条")
+
+latest = st.empty()
+bar = st.progress(0)
+for i in range(100):
+
+    latest.text(f'Iteration {i+1}')
+    bar.progress(i+1)
+    time.sleep(0.1)
+'运行结束'
