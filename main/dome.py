75dd1b4407c11a75d288ebbc3ed3b7eb7fbdfac7
anjo233 <anjo233@outlook.com>
2023-04-07 14:11:15 +0800

    添加文件导入

--- main/dome.py
+++ main/dome.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import  time 
+from PIL import Image
+
 
 st.write("计算进度条")
 
@@ -10,3 +12,15 @@ for i in range(100):
     latest.text(f'Iteration {i+1}')
     bar.progress(i+1)
     time.sleep(0.1)
+"运行结束"
+
+
+upload_file = st.file_uploader(
+    label = "upload image"
+)
+if upload_file is not None:
+    img = Image.open(upload_file)
+    img.show()
+else:
+    st.stop()
+
