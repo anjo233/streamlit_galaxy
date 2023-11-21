1cfa9af6fbbfc641d8f02a344dfa3bc48b01a35d
anjo233 <anjo233@outlook.com>
2023-11-22 02:06:43 +0800

    更新

--- main/dome.py
+++ main/dome.py
@@ -2,19 +2,25 @@ import streamlit as st
 import  time 
 from PIL import Image
 
+st.set_page_config(
+    page_title="Galaxy Classification App",
+    layout="wide"
+)
+st.title("Galaxy Classification")
+
 
 st.write("计算进度条")
 
 latest = st.empty()
 bar = st.progress(0)
-for i in range(10):
+for i in range(100):
 
     latest.text(f'Iteration {i+1}')
     bar.progress(i+1)
-    time.sleep(0.1)
+    time.sleep(10)
 "运行结束"
 
-
+st.subheader("选择图片导入")
 upload_file = st.file_uploader(
     label = "upload image"
 )
